79172213cabf6a9e0fb5b74796aea37c786ec44b
Shoma Takatori <shoma@hogehoge.com>
2023-11-24 14:39:53 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -2,6 +2,16 @@
 # SPDX-FileCopyrightText: 2023 Shoma Takatori
 # SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+      echo NG at Line $1
+      res=1
+}
+
+res=0
+
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 14 ]
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
