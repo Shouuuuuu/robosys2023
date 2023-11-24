2edbba182521a2b0ba8768bfc9efb97a5aaf8839
Shoma Takatori <shoma@hogehoge.com>
2023-11-24 15:28:44 +0900

    Add a commit

--- test.bash
+++ test.bash
@@ -1,24 +1,3 @@
-#!/bin/bash -xv
-# SPDX-FileCopyrightText: 2023 Shoma Takatori
-# SPDX-License-Identifier: BSD-3-Clause
+#!/bin/bash
 
-ng () {
-      echo NG at Line $1
-      res=1
-}
-
-res=0
-
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
-
-out=$(seq あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-[ "$res" = 0 ] && echo OK
-exit $res
+seq 5 | ./plus
