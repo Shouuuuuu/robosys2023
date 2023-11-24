3094b64da228670532458d358486ee05c1b1de8e
Shoma Takatori <shoma@hogehoge.com>
2023-11-24 14:32:52 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -2,20 +2,6 @@
 # SPDX-FileCopyrightText: 2023 Shoma Takatori
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
-       echo NG at Line $1
-       res=1
-}
+out=$(seq 5 | ./plus)
 
-res=0
-
-  out=$(seq 5 | ./plus)
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
-
-  out=$(echo | ./plus)
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
-
-[ "$res" = 0 ] && echo OK       
-  exit $res
+[ "${out}" = 15 ]
